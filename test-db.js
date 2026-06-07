3a668a512ed1a66a61116c4927d28020fdbc18bc
Mahmoud El-Tohamy <mahmoud.eltohamyy@gmail.com>
2026-06-07 22:51:42 +0300

    chore: remove temporary testing and debugging scripts

--- test-db.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const db = mongoose.connection.db;
-  const docs = await db.collection('blacklistentries').find({}).toArray();
-  console.log('Total entries:', docs.length);
-  console.log(JSON.stringify(docs, null, 2));
-  process.exit(0);
-}).catch(console.error);
